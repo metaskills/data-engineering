f631f5da1c6c6519cef54014d7736b003803b439
Ken Collins <ken@metaskills.net>
2012-07-31 17:43:51 -0400

    Hook up a basic form to upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,7 +1,13 @@
 class UploadsController < ApplicationController
 
   def index
-    
+    @upload = Upload.new
+    # @last_10_uploads = Upload.limit(10)
+  end
+
+  def create
+    @upload = Upload.create! params[:upload]
+    redirect_to uploads_url
   end
 
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,4 +2,6 @@ class Upload < ActiveRecord::Base
   
   has_attached_file :file
 
+  attr_accessible :file
+
 end
