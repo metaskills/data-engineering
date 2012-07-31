81e0123da9b80bbac3d53c1daf12410ddc2333c6
Ken Collins <ken@metaskills.net>
2012-07-31 16:22:01 -0400

    Add some validations for PurchaserItem#count

--- app/models/purchaser_item.rb
+++ app/models/purchaser_item.rb
@@ -3,7 +3,8 @@ class PurchaserItem < ActiveRecord::Base
   belongs_to :purchaser
   belongs_to :item
 
-  validates_presence_of :purchaser_id, :item_id
+  validates_presence_of :purchaser_id, :item_id, :count
+  validates_numericality_of :count
   
 
 end
--- test/unit/purchaser_item_test.rb
+++ test/unit/purchaser_item_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class PurchaserItemTest < ActiveSupport::TestCase
 
+  it 'defaults count to 1' do
+    new_purchaser_item.count.must_equal 1
+  end
+
   it 'validates presence of foreign keys' do
     new_purchaser_item.wont_be :valid?
     new_purchaser_item.errors[:purchaser_id].join.must_match %r{can't be blank}
     new_purchaser_item.errors[:item_id].join.must_match %r{can't be blank}
   end
 
+  it 'validates that count is present and an integer' do
+    new_purchaser_item.count = nil
+    new_purchaser_item.wont_be :valid?
+    new_purchaser_item.errors[:count].join.must_match %r{can't be blank}
+    new_purchaser_item.errors[:count].join.must_match %r{not a number}
+  end
 
 
 end
